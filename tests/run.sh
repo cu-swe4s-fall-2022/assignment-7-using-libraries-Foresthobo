9dcbef31a85c6740930d851896b915c1518a7980
Henry Waldstreicher <hjwaldstreicher@gmail.comm>
2022-10-27 21:46:41 +0000

    created run.sh for github actions, edited unit_test for pycodestyle

--- tests/run.sh
+++ tests/run.sh
@@ -5,5 +5,5 @@ set -u # raise error if variable is unset
 set -o pipefail # fail if any prior step failed
 
 pycodestyle $(git ls-files "*.py")
-python -m unittest test_utils.py
+python -m unittest unit_test.py
 bash test_plotter.sh
